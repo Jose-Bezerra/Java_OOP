12dcce8ab29b60e156e2915dda1aca5a2bc2bbc1
José Bezerra <50915483+Jose-Bezerra@users.noreply.github.com>
2023-10-14 18:24:47 -0300

    Update Program.java
    Adicionado o 'Locale' para poder utilizar o ponto nos números decimais sem quebrara a aplicação.

--- application/Program.java
+++ application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
         double xA, xB, xC, yA, yB, yC;
 
